ef12182635dcd757787ec7f82c68174af274607c
Hachounet <hachounet.github@gmx.fr>
2024-09-02 13:53:20 +0200

    Profile layout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -17,9 +17,8 @@ const Logout = () => {
   };
 
   return (
-    <div className="text-xl flex justify-center items-center flex-grow mb-[193.25px]">
-      THIS IS LOGOUT PAGE WIP
-      <span>Click here to log out.</span>
+    <div className="text-xl flex flex-col justify-center items-center flex-grow mb-[193.25px]">
+      <span className="pb-8">Click here to log out.</span>
       <ButtonLogOut
         text="Log out"
         onClickFn={handleClick}
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -13,8 +13,14 @@ export default function Profile({ url }) {
     );
 
   return (
-    <div className="text-xl flex justify-center items-center flex-grow mb-[193.25px]">
-      Your user informations :
+    <div className="text-xl flex flex-col justify-center items-center flex-grow mb-[193.25px] text-center">
+      <span>
+        This page serve no real purpose. <br /> It could be improved to edit
+        your personal data, change your password etc. but this is not project
+        purpose.
+      </span>
+      <br />
+      <span>Your user informations : </span>
       <br />
       Email: {data.user.email}
       <br />
